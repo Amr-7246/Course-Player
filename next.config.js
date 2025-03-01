5b6f9e425547b52cc868e98deec8b9d62768b328
Amr-7246 <aehab7246@gmail.com>
2025-03-01 05:05:35 +0200

    Done

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  rules: {
+    // Custom rules go here (e.g., turn off a rule or configure it)
+  },
+  ignorePatterns: ["node_modules/", ".next/", "out/"],
+};
